51992775d1704dd19b59b202ac0b2446f8d3f457
Developer272024 <157135176+Developer272024@users.noreply.github.com>
2024-01-19 21:44:24 +0100

    Add files via upload

---
+++ Autoverkäufer/fxmanifest.lua.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'Chris'
+description 'Autoverkäufer carspawner'
+version '1.0.0'
+lua54 'yes'
+
+client_scripts {
+'client/client.lua'
+	
+}
+
+server_scripts {
+'server/server.lua'
+
+}
